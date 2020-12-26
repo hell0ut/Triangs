97a5ddadff81b705b92d1fe403efb5532484a21b
hell0ut <43760986+hell0ut@users.noreply.github.com>
2020-12-27 00:40:18 +0200

    Completed, all line changes included

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
+from math import fabs,sqrt
 
 
 def find_distance(circle, point1, point2):
@@ -7,51 +7,67 @@ def find_distance(circle, point1, point2):
     # elif point1[1] == point2[1] and point1[1] == circle[1]:
     #    return False
     # else:
-    distance_y = point1[1] - point2[1]
-    distance_x = point1[0] - point2[0]
-    chisl = math.fabs(distance_y * circle[0] - distance_x * circle[1] + point2[0] * point1[1] - point2[1] * point1[0])
-    znam = math.sqrt(distance_y ** 2 + distance_x ** 2)
+    distance_y = point2[1] - point1[1]
+    distance_x = point2[0] - point1[0]
+    chisl = fabs(distance_y * circle[0] - distance_x * circle[1] + point2[0] * point1[1] - point2[1] * point1[0])
+    znam = sqrt(distance_y ** 2 + distance_x ** 2)
 
     return chisl / znam
 
 
+def perpendicularToLineAllowed(circle,pt1,pt2):
+    line1=(circle[0]-pt1[0])**2+(circle[1]-pt1[1])**2
+    line2=(circle[0]-pt2[0])**2+(circle[1]-pt2[1])**2
+
+    if line1<line2:
+        vec1_x=circle[0]-pt1[0]
+        vec1_y=circle[1]-pt1[1]
+        vec2_x = pt2[0] - pt1[0]
+        vec2_y = pt2[1] - pt1[1]
+    else:
+        vec1_x=circle[0]-pt2[0]
+        vec1_y=circle[1]-pt2[1]
+        vec2_x = pt1[0] - pt2[0]
+        vec2_y = pt1[1] - pt2[1]
+    angle=(vec1_x*vec2_x+vec1_y*vec2_y)/(sqrt(vec1_x**2+vec1_y**2)*sqrt(vec2_x**2+vec2_y**2))
+    return angle>=0
+
+
 def func(rad, circle, points):
-    l = [[math.fabs(circle[0] - t[0]), math.fabs(circle[1] - t[1])] for t in points]
+    l = [[fabs(circle[0] - t[0]), fabs(circle[1] - t[1])] for t in points]
 
     inside = 0
 
     for distance in l:
-        if math.sqrt(distance[0] ** 2 + distance[1] ** 2) == rad:
+        dist= sqrt(distance[0] ** 2 + distance[1] ** 2)
+        if  fabs(dist - rad) < 0.01:
             return "YES"
-        elif math.sqrt(distance[0] ** 2 + distance[1] ** 2) < rad:
+        if dist < rad:
             inside += 1
 
         # else:
         #   outside += 1
         #  outside_m[l.index(distance)] = distance
-    if inside in range(1, 3):
+    if inside == 3:
+        return 'NO'
+    elif inside in range(1, 3):
         return "YES"
-    if inside == 0:
-        i = 0
-        add = 1
-        while i != 2:
-            while i + add != 3:
+    elif inside == 0:
+        for i in range(len(points)):
+            for j in range(i+1,len(points)):
+                if perpendicularToLineAllowed(circle, points[i], points[j]):
+                    d = find_distance(circle, points[i], points[j])
+                    if d <= rad:
+                        return "YES"
+    return 'NO'
 
-                d = find_distance(circle, points[i], points[i + add])
 
-                if d <= rad:  # and d != 0:
 
-                    return "YES"
-                add += 1
-            i += 1
-            add -= 2
-
-    return "NO"
 
 
 for i in range(int(input())):
-    circle_ar = [float(el) for el in input().split()]
+    circle_ar = [int(el) for el in input().split()]
     points = []
-    for i in range(3):
-        points.append([float(el) for el in input().split()])
+    for j in range(3):
+        points.append([int(el) for el in input().split()])
     print(func(circle_ar[2], circle_ar[:2], points))
